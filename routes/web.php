93a24c68b56765445296d73fdeabcde8b006931c
Ahmet Özrahat <ahmetozrahat25@gmail.com>
2022-01-18 17:36:55 +0300

    initial pages and routes have been added.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Home;
+use App\Http\Controllers\AboutUs;
+use App\Http\Controllers\MyOrder;
+use App\Http\Controllers\Contact;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Home::class, 'index']);
+
+Route::get('/aboutus', [AboutUs::class, 'index']);
+
+Route::get('/myorder', [MyOrder::class, 'index']);
+
+Route::get('/contact', [Contact::class, 'index']);
