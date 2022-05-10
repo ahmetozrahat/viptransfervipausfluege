46fc6a432c8bb269211f4297212796689f2d8c54
Ahmet Özrahat <ahmetozrahat25@gmail.com>
2022-05-10 08:24:12 +0300

    little fix.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,7 +7,7 @@
                     {{ __('footer_section_text1') }}
                 </div>
                 <div><img class="footer-tursab-logo" src="{{'img/tursab_logo.svg'}}" width="100" height="100" alt="tursab_logo"></div>
-                <div style="margin-top: 15px;">MARTİNA 07 TURİZM<br>ACENTE BELGE NO: <strong>13995</strong></div>
+                <div class="footer-acente-text">MARTİNA 07 TURİZM<br>ACENTE BELGE NO: <strong>13995</strong></div>
                 <a class="fab fa-facebook-f footer-icn" href="{{$contact->facebook}}" target="_blank"></a>
                 <a class="fab fa-instagram footer-icn" href="{{$contact->instagram}}" target="_blank"></a>
             </div>
