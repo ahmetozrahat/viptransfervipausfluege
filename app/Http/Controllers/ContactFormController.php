e10f43248c323596e2237329634fc3d9e82a3dab
Ahmet Özrahat <ahmetozrahat25@gmail.com>
2022-08-17 11:39:21 +0300

    Sanctum installed for REST API and created API routes.

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -6,6 +6,7 @@ use App\Mail\Contact;
 use App\Models\AdminAccount;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
 
 class ContactFormController extends Controller
@@ -31,6 +32,28 @@ class ContactFormController extends Controller
             Mail::to('info@viptransfervipausfluege.com')
                 ->cc(AdminAccount::where('is_active', true)->get()->all())
                 ->send(new Contact($ticket));
+
+            $ticket = $ticket->fresh();
+
+            $data = [
+                'app_id' => env('ONESIGNAL_APP_ID', ''),
+                'included_segments' => ['Subscribed Users'],
+                'headings' => [
+                    'en' => $ticket->name . ' • Yeni İletişim Talebi'
+                ],
+                'contents' => [
+                    'en' => $ticket->message
+                ],
+                'data' => [
+                    'ticket_id' => $ticket->id
+                ]
+            ];
+
+            Http::contentType('application/json')
+                ->acceptJson()
+                ->withToken(env('ONESIGNAL_REST_API_KEY', ''))
+                ->post('https://onesignal.com/api/v1/notifications', $data);
+
             return true;
         }
         return false;
--- app/Http/Controllers/CreateOrderController.php
+++ app/Http/Controllers/CreateOrderController.php
@@ -6,21 +6,15 @@ use App\Mail\OrderPlaced;
 use App\Mail\OrderPlacedAdmin;
 use App\Models\AdminAccount;
 use App\Models\TransferOrder;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
 
 class CreateOrderController extends Controller
 {
     public function create(Request $request) {
-        /*        $validated = $request->validate([
-            'order_id' => ['string', 'min:12', 'max:12'],
-            'name' => ['string', 'max:255'],
-            'email' => ['string', 'max:255'],
-            'phone' => ['string', 'min:7', 'max:16'],
-            'country' =>
-        ]);*/
-
         if ($this->createOrder($request)) {
             // Row inserted successfully.
             // Send Mail and SMS to both user and admins.
@@ -38,8 +32,28 @@ class CreateOrderController extends Controller
                     ->locale($request->post('lang'))
                     ->send(new OrderPlacedAdmin($order));
 
-                // SMS to the user
-                $smsUser = __(
+                // Send notification for admin panel.
+                $data = [
+                    'app_id' => env('ONESIGNAL_APP_ID', ''),
+                    'included_segments' => ['Subscribed Users'],
+                    'headings' => [
+                        'en' => $order->name . ' • Yeni Sipariş'
+                    ],
+                    'contents' => [
+                        'en' => $order->order_id . ' nolu yeni siparişe göz atmak için dokunun.'
+                    ],
+                    'data' => [
+                        'order_id' => $order->order_id
+                    ]
+                ];
+
+                Http::contentType('application/json')
+                    ->acceptJson()
+                    ->withToken(env('ONESIGNAL_REST_API_KEY', ''))
+                    ->post('https://onesignal.com/api/v1/notifications', $data);
+
+                // SMS to the user (disabled for now.)
+                /*$smsUser = __(
                     'order_confirmation_sms_user',
                     [
                         'name' => $request->post('name'),
@@ -50,7 +64,7 @@ class CreateOrderController extends Controller
                 // SMSController::sendMessage($request->post('phone'), $smsUser);
 
                 // SMS to the admin
-/*                $smsUser = __('order_confirmation_sms_admin', [], $request->post('lang'));
+                $smsUser = __('order_confirmation_sms_admin', [], $request->post('lang'));
                 SMSController::sendMessage('+905441700738', $smsUser);*/
             }else {
                 return false;
