dfb1a0b5882be3a3df7ab0abbd15c76793f69650
Ahmet Özrahat <ahmetozrahat25@gmail.com>
2022-11-03 21:45:35 +0300

    maintainance mode page added.

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VipTransferVipAusfluege | Bakım</title>
+</head>
+
+<body>
+    <div
+        style="margin: auto; display: flex; justify-content: center; align-items: center; height: 100vh; flex-direction: column;">
+        <h1>Bakımdayız</h1>
+        <p>Sizlere daha iyi hizmet sunabilmek için bakım çalışmamız devam ediyor. En kısa sürede tekrar görüşmek
+            dileğiyle.</p>
+    </div>
+</body>
+
+</html>
